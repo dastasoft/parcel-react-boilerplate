8019864aff367e27637944825b738208820578ee
KoshiroKun <dastasoft@protonmail.com>
2020-02-29 11:18:49 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import './index.scss';
+
+const App = () => <div>Hello World!</div>;
+
+render(<App />, document.getElementById('root'));
