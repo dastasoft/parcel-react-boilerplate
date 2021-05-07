593b14223a8bb0cb23b3f03e189240d148c2a1da
dastasoft <dastasoft@protonmail.com>
2021-05-07 17:03:07 +0200

    updated versions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
 
-import './index.scss';
+import './index.css'
 
-const App = () => <>Hello World!</>;
+const App = () => <>Hello World!</>
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'))
